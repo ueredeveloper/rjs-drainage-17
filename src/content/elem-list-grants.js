85bd9338f8e3427c8f79e8074d13e25262e8ae07
ueredeveloper <ueredeveloper@gmail.com>
2023-04-04 13:10:56 -0300

    melhorando a tabela de outorgas

--- src/content/elem-list-grants.js
+++ src/content/elem-list-grants.js
@@ -1,184 +1,502 @@
-import React, { useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import React, { useCallback, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { visuallyHidden } from "@mui/utils";
+//import { points } from "./points";
 
-function createData(
-  us_nome, us_cpf_cnpj, int_processo, emp_endereco, demandas, finalidades) {
-  return {
-    us_nome,
-    us_cpf_cnpj,
-    int_processo,
-    emp_endereco,
-    demandas,
-    finalidades
-  };
-}
-/**
-* Renderizar as finalidades. Para isso verifica se o objeto contém uma array ou não, depois renderiza de acordo com a quantidade de ítens.
-* @param {object} row
-*/
-function renderPurposes(row) {
-  if (row.finalidades !== null) {
-    if (Array.isArray(row.finalidades.finalidades)) {
-      return row.finalidades.finalidades.map((fin, index) => (
-        <TableRow key={index}>
-          <TableCell component="th" scope="row">
-            {fin.id_finalidade}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {fin.descricao}
-          </TableCell>
-          <TableCell>{fin.vazao}</TableCell>
-          <TableCell >{fin.id_interferencia}</TableCell>
-          <TableCell >
-            {/*Math.round(fin.amount * row.price * 100) / 100*/}
-          </TableCell>
-        </TableRow>
-
-      ))
-    } else {
-      return (
-        <TableRow>
-          <TableCell component="th" scope="row">
-            {row.finalidades.finalidades.id_finalidade}
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {row.finalidades.finalidades.descricao}
-          </TableCell>
-          <TableCell>{row.finalidades.finalidades.vazao}</TableCell>
-          <TableCell >{row.finalidades.finalidades.id_interferencia}</TableCell>
-          <TableCell >
-            {/*Math.round(fin.amount * row.price * 100) / 100*/}
-          </TableCell>
-        </TableRow>
-      )
-    }
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
   }
-
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
 }
 
-function Row(props) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: "us_nome",
+    numeric: false,
+    disablePadding: true,
+    label: "Nome"
+  },
+  {
+    id: "us_cpf_cnpj",
+    numeric: false,
+    disablePadding: false,
+    label: "CPF/CNPJ"
+  },
+  {
+    id: "int_processo",
+    numeric: false,
+    disablePadding: false,
+    label: "Processo"
+  },
+  {
+    id: "emp_endereco",
+    numeric: false,
+    disablePadding: false,
+    label: "Endereço"
+  },
+  {
+    id: "jan",
+    numeric: false,
+    disablePadding: false,
+    label: "Jan"
+  },
+  {
+    id: "fev",
+    numeric: false,
+    disablePadding: false,
+    label: "Fev"
+  },
+  {
+    id: "mar",
+    numeric: false,
+    disablePadding: false,
+    label: "Mar"
+  },
+  {
+    id: "abr",
+    numeric: false,
+    disablePadding: false,
+    label: "Abr"
+  },
+  {
+    id: "mai",
+    numeric: false,
+    disablePadding: false,
+    label: "Mai"
+  },
+  {
+    id: "jun",
+    numeric: false,
+    disablePadding: false,
+    label: "Jun"
+  },
+  {
+    id: "jul",
+    numeric: false,
+    disablePadding: false,
+    label: "Jul"
+  },
+  {
+    id: "ago",
+    numeric: false,
+    disablePadding: false,
+    label: "Ago"
+  },
+  {
+    id: "set",
+    numeric: false,
+    disablePadding: false,
+    label: "Set"
+  },
+  {
+    id: "out",
+    numeric: false,
+    disablePadding: false,
+    label: "Out"
+  },
+  {
+    id: "nov",
+    numeric: false,
+    disablePadding: false,
+    label: "Nov"
+  },
+  {
+    id: "dez",
+    numeric: false,
+    disablePadding: false,
+    label: "Dez"
+  }
+];
+
+const DEFAULT_ORDER = "asc";
+const DEFAULT_ORDER_BY = "us_nome";
+const DEFAULT_ROWS_PER_PAGE = 100;
+
+function EnhancedTableHead(props) {
+  const {
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort
+  } = props;
+  const createSortHandler = (newOrderBy) => (event) => {
+    onRequestSort(event, newOrderBy);
+  };
 
   return (
-    <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        {/* icon down up*/}
-        <TableCell>
-          <IconButton color="secondary"
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        {/** points */}
-        <TableCell >{row.us_nome}</TableCell>
-        <TableCell >{row.us_cpf_cnpj}</TableCell>
-        <TableCell >{row.int_processo}</TableCell>
-        <TableCell >{row.emp_endereco}</TableCell>
-        {/** vazões */}
-        {
-          row.demandas.demandas.map((dem, i) => {
-            return (
-              <TableCell key={i} >{parseFloat(dem.vol_mensal_mm).toFixed(2)}</TableCell>
-            )
-          })
-        }
-      </TableRow>
+    <TableHead>
       <TableRow>
-        {/** finalidades */}
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              {/* Detalhes - Título */}
-              <Typography variant="h6" gutterBottom component="div">
-                Finalidades
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                {/* th - table head */}
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Id Finalidade</TableCell>
-                    <TableCell>Finalidade</TableCell>
-                    <TableCell >Vazão</TableCell>
-                    <TableCell >Id Interferência</TableCell>
-                  </TableRow>
-                </TableHead>
-                {/*tbody - table body */}
-                <TableBody>
-                  {renderPurposes(row)}
-                </TableBody>
-
-              </Table>
-            </Box>
-          </Collapse>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              "aria-label": "select all desserts"
+            }}
+          />
         </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
       </TableRow>
-    </React.Fragment>
+    </TableHead>
   );
 }
 
-function ElemListGrants({ points }) {
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired
+};
 
-  const [rows, setRows] = useState([createData('', '', '', '', { demandas: [] }, { finalidades: [] })])
+function EnhancedTableToolbar(props) {
+  const { numSelected } = props;
 
-  const _setRows = (points) => {
-    let _points = points.map(o => {
-      return createData(
-        o.us_nome,
-        o.us_cpf_cnpj,
-        o.int_processo,
-        o.emp_endereco,
-        o.demandas,
-        o.finalidades
-      )
-    })
-    setRows(_points);
-  }
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            )
+        })
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Outorgas
+        </Typography>
+      )}
 
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+}
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired
+};
+
+export default function ElemListGrants({ points }) {
+  const [order, setOrder] = useState(DEFAULT_ORDER);
+  const [orderBy, setOrderBy] = useState(DEFAULT_ORDER_BY);
+  const [selected, setSelected] = useState([]);
+  const [page, setPage] = useState(0);
+  const [dense, setDense] = useState(false);
+  const [visibleRows, setVisibleRows] = useState(null);
+  const [rowsPerPage, setRowsPerPage] = useState(DEFAULT_ROWS_PER_PAGE);
+  const [paddingHeight, setPaddingHeight] = useState(0);
+
+  const [rows, setRows] = useState(points);
 
   useEffect(() => {
-    if (points.length !== 0) _setRows(points);
-  }, [points]);
+    let rowsOnMount = stableSort(
+      rows,
+      getComparator(DEFAULT_ORDER, DEFAULT_ORDER_BY)
+    );
+
+    rowsOnMount = rowsOnMount.slice(
+      0 * DEFAULT_ROWS_PER_PAGE,
+      0 * DEFAULT_ROWS_PER_PAGE + DEFAULT_ROWS_PER_PAGE
+    );
+
+    setVisibleRows(rowsOnMount);
+  }, [rows]);
+
+  useEffect(() => {
+    setRows(points)
+  }, [points])
+
+  const handleRequestSort = useCallback(
+    (event, newOrderBy) => {
+      const isAsc = orderBy === newOrderBy && order === "asc";
+      const toggledOrder = isAsc ? "desc" : "asc";
+      setOrder(toggledOrder);
+      setOrderBy(newOrderBy);
+
+      const sortedRows = stableSort(
+        rows,
+        getComparator(toggledOrder, newOrderBy)
+      );
+      const updatedRows = sortedRows.slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      );
+
+      setVisibleRows(updatedRows);
+    },
+    [order, orderBy, page, rowsPerPage, rows]
+  );
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n.us_nome);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = useCallback(
+    (event, newPage) => {
+      setPage(newPage);
+
+      const sortedRows = stableSort(rows, getComparator(order, orderBy));
+      const updatedRows = sortedRows.slice(
+        newPage * rowsPerPage,
+        newPage * rowsPerPage + rowsPerPage
+      );
+
+      setVisibleRows(updatedRows);
+
+      // Avoid a layout jump when reaching the last page with empty rows.
+      const numEmptyRows =
+        newPage > 0
+          ? Math.max(0, (1 + newPage) * rowsPerPage - rows.length)
+          : 0;
+
+      const newPaddingHeight = (dense ? 33 : 53) * numEmptyRows;
+      setPaddingHeight(newPaddingHeight);
+    },
+    [order, orderBy, dense, rowsPerPage]
+  );
+
+  const handleChangeRowsPerPage = useCallback(
+    (event) => {
+      const updatedRowsPerPage = parseInt(event.target.value, 10);
+      setRowsPerPage(updatedRowsPerPage);
+
+      setPage(0);
+
+      const sortedRows = stableSort(rows, getComparator(order, orderBy));
+      const updatedRows = sortedRows.slice(
+        0 * updatedRowsPerPage,
+        0 * updatedRowsPerPage + updatedRowsPerPage
+      );
+
+      setVisibleRows(updatedRows);
+
+      // There is no layout jump to handle on the first page.
+      setPaddingHeight(0);
+    },
+    [order, orderBy]
+  );
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
 
   return (
-    <TableContainer sx={{ maxHeight: 330 }} component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Nome</TableCell>
-            <TableCell>CPF/CNPJ</TableCell>
-            <TableCell>Processo</TableCell>
-            <TableCell >Endereço</TableCell>
-            {['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'].map((month, i) => {
-              return (
-                <TableCell key={i}>{month}</TableCell>
-              )
-            })}
-          </TableRow>
-        </TableHead>
-        {<TableBody>
-          {rows.map((row, i) => (
-            <Row key={i} row={row} />
-          ))}
-        </TableBody>}
-      </Table>
-    </TableContainer>
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer sx={{ maxHeight: 330 }}>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? "small" : "medium"}
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {visibleRows
+                ? visibleRows.map((row, index) => {
+                  const isItemSelected = isSelected(row.us_nome);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.us_nome)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={index}
+                      selected={isItemSelected}
+                      sx={{ cursor: "pointer" }}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            "aria-labelledby": labelId
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row.us_nome}
+                      </TableCell>
+                      <TableCell align="right">{row.us_cpf_cnpj}</TableCell>
+                      <TableCell align="right">{row.int_processo}</TableCell>
+                      <TableCell align="right">{row.emp_endereco}</TableCell>
+                      {row.demandas.demandas.map((dem, i) => {
+                        return (
+                          <TableCell key={i}>
+                            {parseFloat(dem.vol_mensal_mm).toFixed(2)}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })
+                : null}
+              {paddingHeight > 0 && (
+                <TableRow
+                  style={{
+                    height: paddingHeight
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[50, 100, 150, 300, 350, 400]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
   );
 }
-export default ElemListGrants;
