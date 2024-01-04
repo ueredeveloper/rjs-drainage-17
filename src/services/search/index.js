e75f291f528da6f58b2d28d2ba925d3a9334af09
Replit user <>
2024-01-04 04:27:13 +0000

    fix: update server link

--- src/services/search/index.js
+++ src/services/search/index.js
@@ -1,4 +1,5 @@
-const url = 'https://njs-drainage.ueredeveloper.repl.co';
+//const url = 'https://njs-drainage.ueredeveloper.repl.co';
+const url = 'https://ec96a2d0-8ba3-41ec-9211-9dbcf7faee95-00-1dq8pdj62qrea.hacker.replit.dev'
 
 /**
 * Buscar a shape solicitada no servidor
--- src/services/shapes/index.js
+++ src/services/shapes/index.js
@@ -1,5 +1,5 @@
-
-const url = 'https://njs-drainage.ueredeveloper.repl.co';
+//const url = 'https://njs-drainage.ueredeveloper.repl.co';
+const url = 'https://ec96a2d0-8ba3-41ec-9211-9dbcf7faee95-00-1dq8pdj62qrea.hacker.replit.dev'
 
 /**
  * Função assíncrona que encontra pontos dentro de um polígono.
